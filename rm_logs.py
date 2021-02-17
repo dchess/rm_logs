70b3a49506c8fb7076177551b67f5861694d1a1f
dhess <dan.hess@kippnorcal.org>
2021-02-17 14:57:50 -0800

    Refactor main

--- rm_logs.py
+++ rm_logs.py
@@ -5,43 +5,62 @@ import logging
 import os
 import sys
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--folder", help="Log folder path")
 parser.add_argument("--debug", help="Enable debug logging", action="store_true")
 parser.add_argument(
     "--days", type=int, default=30, help="Delete logs older than x days"
 )
-args, _ = parser.parse_known_args()
+ARGS, _ = parser.parse_known_args()
 
-FOLDER = args.folder
-DAYS = args.days
-MB = 1024 * 1024
 
 logging.basicConfig(
     handlers=[logging.StreamHandler(sys.stdout),],
-    level=logging.DEBUG if args.debug else logging.INFO,
+    level=logging.DEBUG if ARGS.debug else logging.INFO,
     format="%(asctime)s | %(levelname)s: %(message)s",
     datefmt="%Y-%m-%d %I:%M:%S%p %Z",
 )
 
-log_files = sorted(os.listdir(FOLDER))
 
-removal_date = datetime.date.today() - datetime.timedelta(days=DAYS)
-deleted_logs = 0
-disk_space = 0
-for log in log_files:
-    log_date, _ = log.split("_")
-    log_date = datetime.datetime.strptime(log_date, "%Y%m%d").date()
-    if log_date < removal_date:
-        file = os.path.join(FOLDER, log)
-        filesize = os.stat(file).st_size
-        os.remove(file)
-        logging.debug(f"Removed {log}")
-        deleted_logs += 1
-        disk_space += filesize
+def file_datestamp(file):
+    """Pull datestamp from filename or use modified date if not present"""
+    try:
+        name = os.path.basename(file)
+        date_str, _ = name.split("_")
+        log_date = datetime.datetime.strptime(date_str, "%Y%m%d").date()
+    except ValueError:
+        mtime = os.stat(file).st_mtime
+        log_date = datetime.datetime.fromtimestamp(mtime).date()
+    return log_date
 
-disk_space = round(disk_space / MB, 3)
-logging.info(
-    f"Removed {deleted_logs} log files older then {removal_date} which used {disk_space} MB"
-)
 
+def convert_size(size_bytes):
+    """Convert filesize in bytes to human readable units"""
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return f"{s}{size_name[i]}"
+
+
+def rm_old_logs(logs):
+    """Delete log files older than a specified date"""
+    rm_date = datetime.date.today() - datetime.timedelta(days=ARGS.days)
+    deleted = 0
+    space = 0
+    for log in logs:
+        if file_datestamp(log) < rm_date:
+            space += os.stat(log).st_size
+            os.remove(log)
+            deleted += 1
+            logging.debug(f"Removed {log}")
+    space = convert_size(space)
+    logging.info(f"Removed {deleted} logs ({space}) older than {rm_date}")
+
+
+if __name__ == "__main__":
+    logs = glob.glob(f"{ARGS.folder}/*.log")
+    rm_old_logs(logs)
